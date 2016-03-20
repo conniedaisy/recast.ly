270cfe7b890e0f68c363dde66434a540ec4ceb6e
conniedaisy <conniedcheung@gmail.com>
2016-03-19 17:29:33 -0700

    Begin setting up Youtube API

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,18 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
